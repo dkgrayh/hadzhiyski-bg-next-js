2e03b53b0388b6a7b95335d668f3415e9827a9a4
Nikolay Hadzhiyski <nikolay.hadzhiyski@gmail.com>
2024-09-18 12:49:27 -0400

    Clean up

--- src/app/api/subscribe/route.ts
+++ src/app/api/subscribe/route.ts
@@ -1,15 +1,17 @@
 import { NextRequest, NextResponse } from 'next/server';
-
 import Subscriber from '@/models/Subscriber';
 import connectToDatabase from '@/lib/mongodb';
 
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json(); // Parse the incoming JSON
+    console.log('Request Body:', body); // Log the request body
+
     const { email } = body;
 
     // Validate the email is present in the request
     if (!email) {
+      console.log('Error: Email is required');
       return NextResponse.json(
         { message: 'Failed', error: 'Email is required' },
         { status: 400 }
@@ -23,6 +25,7 @@ export async function POST(req: NextRequest) {
     const existingEmail = await Subscriber.findOne({ email });
 
     if (existingEmail) {
+      console.log('Error: Email is already subscribed');
       return NextResponse.json(
         { message: 'Failed', error: 'Email is already subscribed' },
         { status: 400 }
@@ -34,6 +37,7 @@ export async function POST(req: NextRequest) {
     await newSubscriber.save();
 
     // Return success response
+    console.log('Subscription successful');
     return NextResponse.json(
       { message: 'Subscription successful' },
       { status: 200 }
